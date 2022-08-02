490150a0842f7130e9b3433628ffa580e49e20c1
da2i2 <110165348+da2i2@users.noreply.github.com>
2022-08-01 22:44:53 -0900

    Update test_me.py

--- test_me.py
+++ test_me.py
@@ -120,7 +120,7 @@ with open('device_list_s2.txt', 'a') as f:
 print("================== Stress Test Done ====================")
 
 print(Fore.GREEN + "============================================")
-print("PASSWORD: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")
+print("PASSWORD: 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")
 print("============================================" + Fore.RESET)
 
 
